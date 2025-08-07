07012cd77aa6bc73c46209fc1b82c6b503723b45
Terence Chardes <terence.chardes@gmail.com>
2025-08-07 18:55:32 +0200

    recentralization + readme

---
+++ build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_mazeGenerator_FILE /home/terence/0-documents/0-perso/1-projets-non-executables/QTProjects/mazeGenerator_wallpaper/build/Desktop_Qt_6_9_1-Debug/mazeGenerator)
+set(__QT_DEPLOY_TARGET_mazeGenerator_TYPE EXECUTABLE)
---
+++ build/Desktop_Qt_6_9_1-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles/mazeGenerator_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/mazeGenerator_autogen.dir/ParseCache.txt"
+  "mazeGenerator_autogen"
+  )
+endif()
